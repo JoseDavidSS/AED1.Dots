e6515fc6d8823de2366b14d19c46c58b2a26c351
Apu <jp.1398@hotmail.com>
2018-08-26 21:12:04 -0600

    Añade la clase servidor

---
+++ src/com/dots/server/server.java
@@ -0,0 +1,22 @@
+package com.dots.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+    public void start() throws IOException {
+        ServerSocket server = new ServerSocket(10000);
+        while (true) {
+            Socket socket = server.accept();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = reader.readLine();
+            System.out.println(line);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("ok");
+        }
+    }
+}
