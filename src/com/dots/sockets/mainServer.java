252506883b07159a5f8a187e68a921e36afe2451
Apu <jp.1398@hotmail.com>
2018-09-02 00:56:34 -0600

    Agrega codigo main

--- src/com/dots/sockets/mainServer.java
+++ src/com/dots/sockets/mainServer.java
@@ -1,7 +1,16 @@
 package com.dots.sockets;
 
+import java.io.IOException;
+
 public class mainServer {
 
+    public static void main(String[] args) {
+        Server s1 = new Server();
+        try {
+            s1.start();
+        } catch (IOException e) {
+            System.out.println("Mae se despicho la vara");
+        }
 
-
+    }
 }
