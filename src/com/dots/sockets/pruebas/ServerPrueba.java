f211e6dfb5b2feed7eb2d595164ac7c98d9dc2d2
Kevin Cordero <38987651+Skryfall@users.noreply.github.com>
2018-09-09 13:17:19 -0600

    Añadido soporte de JSON de prueba, solo falta implementarlo correctamente

--- src/com/dots/sockets/pruebas/ServerPrueba.java
+++ src/com/dots/sockets/pruebas/ServerPrueba.java
@@ -1,21 +1,29 @@
 package com.dots.sockets.pruebas;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ServerPrueba {
 
     public static void main(String[] args) throws IOException{
         ServerSocket socket = new ServerSocket(50099);
+        ObjectMapper mapper = new ObjectMapper();
         while(true){
             Socket incoming = socket.accept();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
             try{
-                String lineaOriginal = reader.readLine();
-                System.out.println(lineaOriginal);
+                String lineaOriginal = in.readLine();
+                System.out.println("El cliente mando: " + lineaOriginal);
+                PrintWriter out = new PrintWriter(incoming.getOutputStream(), true);
+                out.println("xd");
+                out.println();
+                String json = in.readLine();
+                System.out.println(json);
+                DatosLineaS l1 = mapper.readValue(json, DatosLineaS.class);
+                System.out.println(l1.getPosxi() + l1.getPosxf());
             }catch (IOException ex){
                 ex.printStackTrace();
             }
