5eb769385b69c16a2b95958073ae9455ae4b45c5
JoseDavidSS <39009363+JoseDavidSS@users.noreply.github.com>
2018-09-08 19:23:13 -0600

    Se crea la aplicación de JavaFX con Scene Builder para agilizar la creación de la interfaz.

--- src/com/dots/client/menu/VentanaJuego.java
+++ src/com/dots/client/menu/VentanaJuego.java
@@ -1,54 +1,52 @@
 package com.dots.client.menu;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 
-public class VentanaJuego extends Application {
+public class VentanaJuego extends Application implements EventHandler<ActionEvent> {
 
-    Stage juego;
-    Scene Tablero;
-    Scene ventana3x3;
+    Button boton3x3;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 
-    public void start(Stage stagePrimario){
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Title of Window");
+        Label label1 = new Label("Tamaño de tablero");
+        boton3x3 = new Button();
+        boton3x3.setText("3x3");
 
-        juego = stagePrimario;
-
-        Label label1 = new Label("Tamaño de Tablero:");
-        Button boton1 = new Button("3x3");
-        boton1.setOnAction(e -> juego.setScene(ventana3x3));
-
-        Button boton2 = new Button("5x5");
-        boton1.setOnAction(e -> juego.setScene(ventana3x3));
-
-        Button boton3 = new Button("7x7");
-        boton1.setOnAction(e -> juego.setScene(ventana3x3));
+        //This class will handle the button events
+        boton3x3.setOnAction(this);
 
         VBox orden1 = new VBox(20);
-        orden1.getChildren().addAll(label1, boton1, boton2, boton3);
+        orden1.setLayoutX(100);
+        orden1.setLayoutY(100);
+        orden1.getChildren().addAll(label1, boton3x3);
+        Scene scene = new Scene(orden1, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-        Tablero = new Scene(orden1, 400, 400);
+    //When button is clicked, handle() gets called
+    //Button click is an ActionEvent (also MouseEvents, TouchEvents, etc...)
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == boton3x3)
+            System.out.println("Se crea tablero 3x3");
+        else if (event.getSource() == boton3x3){
 
-        Button boton5 = new Button("Volver");
-        boton2.setOnAction(e -> juego.setScene(Tablero));
-
-        HBox orden2 = new HBox(20);
-        orden2.getChildren().addAll(boton5);
-        ventana3x3 = new Scene(orden2, 400, 400);
-
-        juego.setScene(Tablero);
-        juego.setTitle("Dots");
-        juego.show();
-
+        }
     }
 
 }
