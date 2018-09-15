04fa6eef98a2378dd86f6caf6d82f0d08a17a57a
JoseDavidSS <39009363+JoseDavidSS@users.noreply.github.com>
2018-09-15 12:55:25 -0600

    Se documenta la clase controller Juego, sus atributos y sus respectivos métodos.

--- src/com/dots/client/menu/Boton.java
+++ src/com/dots/client/menu/Boton.java
@@ -9,11 +9,21 @@ public class Boton extends Button {
     private double coordenadaX;
     private double coordenadaY;
 
+    /**
+     * Es te es el constructor de la clase Boton.
+     * @param ejeX variable que almacena la posición en X del botón.
+     * @param ejeY variable que almacena la posición en X del botón.
+     */
     public Boton (double ejeX, double ejeY){
-
         this.boton = crearBoton(ejeX, ejeY);
     }
 
+    /**
+     * crearBoton es una función para crear un botón con características diferentes a los botones normales.
+     * @param ejeX atributo para asignar la posición en X del botón.
+     * @param ejeY atributo para asignar la posición en Y del botón.
+     * @return
+     */
     private Button crearBoton(double ejeX, double ejeY) {
 
         double r = 6.0;
@@ -30,7 +40,11 @@ public class Boton extends Button {
         return btn;
     }
 
+    /**
+     * presionarBoton es una funcion dedicada a ser llamada por el setOnAction de cada botón creado e imprime su posición en X y en Y.
+     */
     private void presionarBoton (){
+
         System.out.println(coordenadaX +","+coordenadaY);
 
 
